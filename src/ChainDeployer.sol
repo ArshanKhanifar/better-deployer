3a6098b5b698256ddb6faf007baeeb30219b8b62
arshan <arshankhanifar@gmail.com>
2023-12-27 15:41:56 -0500

    added deployer

--- src/ChainDeployer.sol
+++ src/ChainDeployer.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import {BetterDeployer} from "./BetterDeployer.sol";
+import {BaseChainSetup} from "forge-toolkit/BaseChainSetup.sol";
 
-contract ChainDeployer is BetterDeployer {
+contract ChainDeployer is BetterDeployer, BaseChainSetup {
     function _name(
         string memory chain,
         string memory deploymentName
